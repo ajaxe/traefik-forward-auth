e797da6071d76b5dddc3fa7ad6cae010b9702635
Ajay Sharma <ajay.sharma1984@gmail.com>
2024-08-10 18:17:10 -0400

    Hashing password if in plain text

--- src/Services/AppAuthService.cs
+++ src/Services/AppAuthService.cs
@@ -1,15 +1,20 @@
+using System.Runtime.Intrinsics.Arm;
 using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.Json;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using MongoDB.Bson;
 using TraefikForwardAuth.Helpers;
+using ZstdSharp.Unsafe;
 
 namespace TraefikForwardAuth.Services;
 
 public class AppAuthService : IAuthService
 {
+    private const int SaltByteLength = 10;
     private readonly IHostedApplicationService appService;
     private readonly AppDbContext dbContext;
     private readonly ILogger<AppAuthService> logger;
@@ -25,10 +30,25 @@ public class AppAuthService : IAuthService
     public async Task<AuthenticationResult> Authenticate(string username, string password)
     {
         // add password hashing later
-        var existing = await dbContext.AppUsers.AsNoTracking()
-            .FirstOrDefaultAsync(u => u.UserName == username && u.Password == password);
+        var existing = await dbContext.AppUsers
+            .FirstOrDefaultAsync(u => u.UserName == username);
 
-        if (existing is not null)
+        var passwordMatch = false;
+
+        if (existing?.Password == password)
+        {
+            logger.LogInformation("Password match, but need hashing.");
+            existing.Password = HashPassword(existing.Password);
+            await dbContext.SaveChangesAsync();
+
+            passwordMatch = true;
+        }
+        else
+        {
+            passwordMatch = ValidatePassword(existing, password);
+        }
+
+        if (existing is not null && passwordMatch)
         {
             return new AuthenticationResult
             {
@@ -50,6 +70,42 @@ public class AppAuthService : IAuthService
                 Success = false,
             };
         }
+    }
+
+    private bool ValidatePassword(AppUser? existing, string password)
+    {
+        _ = existing ?? throw new ArgumentNullException(nameof(existing));
+
+        var allBtyes = Base64UrlTextEncoder.Decode(existing.Password);
+        var salt = allBtyes.Take(SaltByteLength).ToArray();
+        var cipher = allBtyes.Skip(SaltByteLength).ToArray();
+
+        var rehash = GeneratePasswordHash(password, salt);
+
+        return cipher.SequenceEqual(rehash);
+    }
+
+    private string HashPassword(string password)
+    {
+        var salt = new byte[SaltByteLength];
+        RandomNumberGenerator.Fill(salt);
+        byte[] hashBytes;
+
+        hashBytes = GeneratePasswordHash(password, salt);
+
+        return Base64UrlTextEncoder.Encode(salt.Concat(hashBytes).ToArray());
+    }
+
+    private static byte[] GeneratePasswordHash(string password, byte[] salt)
+    {
+        byte[] hashBytes;
+        using (var hash = SHA256.Create())
+        {
+            // hash salt + password bytes
+            hashBytes = hash.ComputeHash(salt.Concat(Encoding.UTF8.GetBytes(password)).ToArray());
+        }
+
+        return hashBytes;
     }
 
     public async Task<string> AuthCheck(AuthCheckData authCheckData)
