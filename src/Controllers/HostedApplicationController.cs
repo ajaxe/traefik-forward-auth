496846974c9d000fc4f8ca62d72e43491eab0483
Ajay Sharma <ajay.sharma1984@gmail.com>
2024-08-10 20:02:32 -0400

    Add login to auth manager

--- src/Controllers/HostedApplicationController.cs
+++ src/Controllers/HostedApplicationController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TraefikForwardAuth.Models;
 
 namespace TraefikForwardAuth.Controllers;
 
+[Authorize]
 public class HostedApplicationController : Controller
 {
     private readonly IHostedApplicationService hostedAppService;
